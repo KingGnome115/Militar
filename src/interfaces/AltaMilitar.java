e99697a3fe71a11fd021dfb0d8dd8dfeb2d24783
KingGnomo115 <kevinjec115@gmail.com>
2022-03-29 22:35:34 -0600

    Sin comentarios

--- src/interfaces/AltaMilitar.java
+++ src/interfaces/AltaMilitar.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interfaces;
 
 import javax.swing.JOptionPane;
 import militar.CartillaMilitar;
 import militar.Nodo;
 
-/**
- *
- * @author Kevin
- */
 public class AltaMilitar extends javax.swing.JFrame
 {
 
--- src/interfaces/Index.java
+++ src/interfaces/Index.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interfaces;
 
 import militar.ColasD;
-import cjb.ci.Validaciones;
 import javax.swing.JOptionPane;
 import militar.Bicolas;
 import militar.CartillaMilitar;
 import militar.Nodo;
 import militar.PilasD;
 
-/**
- *
- * @author Kevin
- */
 public class Index extends javax.swing.JFrame
 {
 
